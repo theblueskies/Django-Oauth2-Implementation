179ab1c38d9e8727e52c01fe73a38b68d1acef32
Blade <druhin.bala@gmail.com>
2016-04-17 22:06:55 -0700

    more files

---
+++ authorize/api_access_example.py
@@ -0,0 +1,34 @@
+import requests
+import json
+from django.http import Http404
+CLIENT_ID = '123'
+CLIENT_SECRET = '456'
+
+def get_token():
+  response = requests.post(
+    'http://127.0.0.1:8000/o/token/',
+    data={
+        'grant_type': 'password',
+        'username': 'bachuta',
+        'password': 'bachuta',
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET
+      }
+  )
+  return response.text
+
+def get_page():
+  try:
+    token = json.loads(get_token())
+    print ('Bearer ' + token['access_token'])
+    r = requests.get(
+      'http://127.0.0.1:8000/dummy',
+      headers={
+      'Authorization': ('Bearer ' + token['access_token'])
+      }
+    )
+    print r.text
+  except:
+    raise Http404
+
+get_page()
---
+++ authorize/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from rest_framework import generics
+from app_permissions import IsAuthenticatedOrCreate
+from serializers import SignUpSerializer
+from rest_framework import permissions
+from django.http import Http404 
+
+# Create your views here.
+
+# This endpoint is protected. You can access it by passing in the token with the GET request.
+class DummyResponse(APIView):
+  permission_classes = [permissions.IsAuthenticated]
+  def get(self, request, *args, **kwargs):
+    usernames = [user.username for user in User.objects.all()]
+    return Response(usernames)
+
+# This endpoint will allow all access for signing-up.
+class SignUp(generics.CreateAPIView):
+  queryset = User.objects.all()
+  serializer_class = SignUpSerializer
+  permission_classes = (IsAuthenticatedOrCreate,)
+
